cee5892da5300f6aafb85f4e30d4371b31fc1f62
beatrizveloso <beatrizoslov6@gmail.com>
2024-10-08 10:25:11 -0300

    feat: second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function escolha(){
     var numero = prompt("Insira um número ")
+    document.write ("<select multiple style='height:400px; width:300px'> ")
+    var resto;
+    for (var n1=0; n1 <= numero; n1++){
+        resto = n1 % 2;
+        if (resto == 0){
+            
+        }
+document.write ("<option>" + numero + "x" + n1 + "=" + (numero * n1) + "</option>" )
+    
+    }
+    document.write ("</select>")
+    document.write ("<br><br><input type= 'button' value='volta' onclick'volta()'>")
+}
+function volta(){
+    window.location.reload(true);
 }
